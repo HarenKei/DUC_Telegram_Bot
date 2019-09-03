08e9737fb67c33f946cdf48932985dadd23cd718
[HarenKei] <seungmin1266@gmail.com>
2019-09-03 10:29:10 +0900

    requests 이용한 json 데이터 파싱

--- DUC_bot.py
+++ DUC_bot.py
@@ -1,4 +1,11 @@
 import telegram
+import requests
+
+#api key : cba6316cc66b10b108bc4dda1c1cacf1
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=cba6316cc66b10b108bc4dda1c1cacf1'
+
+data = requests.get(url).json()
 
 my_token = '825907431:AAH_3LTZ40DKGSJivCXKcSjAEErjtCcb2U8'
 
@@ -9,4 +16,4 @@ updates = bot.getUpdates()
 for u in updates :
     print(u.message)
 
-
+print(data)
